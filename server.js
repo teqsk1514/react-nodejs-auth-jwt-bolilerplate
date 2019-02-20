211f0304b26f28d1dc32eaa031e6d1f2661f55ca
Ravi raj <ravisk1514@gmail.com>
2019-02-20 14:43:41 +0530

    make it better

--- server.js
+++ server.js
@@ -33,26 +33,26 @@ require("./services/passport")(passport);
 // use routes
 app.use("/api/users", userRoute);
 
-// if (process.env.NODE_ENV == "production") {
-//   // express will serve up production assets
-//   // like our main.js file,or main.css file.
-//   app.use(express.static("client/build"));
-//   // express will serve up the index.html file
-//   // if it doesnot recognize the route.
-//   const path = require("path");
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
-
-//production mode
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
-  //
+if (process.env.NODE_ENV == "production") {
+  // express will serve up production assets
+  // like our main.js file,or main.css file.
+  app.use(express.static("client/build"));
+  // express will serve up the index.html file
+  // if it doesnot recognize the route.
+  const path = require("path");
   app.get("*", (req, res) => {
-    res.sendfile(path.join((__dirname = "client/build/index.html")));
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+//production mode
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client/build")));
+//   //
+//   app.get("*", (req, res) => {
+//     res.sendfile(path.join((__dirname = "client/build/index.html")));
+//   });
+// }
 
 // initialoze the port.
 const PORT = process.env.PORT || 5000;
